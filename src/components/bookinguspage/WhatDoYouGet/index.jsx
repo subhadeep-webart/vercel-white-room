a3ebfa03cf8ba697ff066194928f898e4c86e1a5
subhadeep-webart <subhadeep.das@webart.technology>
2026-01-14 10:30:12 +0530

    WR positioning completed

--- src/components/bookinguspage/WhatDoYouGet/index.jsx
+++ src/components/bookinguspage/WhatDoYouGet/index.jsx
@@ -34,43 +34,6 @@ const WhatDoYouGet = ({ getWithBoking }) => {
 
   return (
     <>
-      {/* <section className="bg-black relative overflow-hidden">
-        <NoiseComponent/>
-        <div className="container">
-          <div className="relative h-[750px]">
-            <div className="absolute left-[67px] top-[-170px] md:top-0 w-[577px] md:w-[1010px]">
-              <Image
-                src={getWithBoking?.section_image_url}
-                alt=""
-                width={1010}
-                height={747}
-                className="w-full h-auto img_border"
-                priority
-              />
-            </div>
-
-            <div className="w-[675px] md:w-[875px] h-[520px] md:h-[600px] bg-[#1B1E25] shadow-xl p-7 md:p-10 absolute bottom-[66px] right-[-372px] transform -translate-x-1/2 z-10">
-              <h3 className="text-white font-bold text-xl md:text-4xl mb-6">
-                {getWithBoking?.title}
-              </h3>
-        
-              <div
-                className="text-base md:text-lg font-normal text-[#8F8F8F] mb-8"
-                dangerouslySetInnerHTML={{
-                  __html: renderContentWithArrows(getWithBoking?.description),
-                }}
-              />
-            </div>
-          </div>
-
-          <div className=" md:mt-8">
-            <p className="text-base md:text-lg font-normal text-white text-center">
-              Take a detailed look at how your big night will play out on our
-              ‘how it works on the day’ page.
-            </p>
-          </div>
-        </div>
-      </section> */}
       <section className="bg-black relative overflow-hidden">
         <NoiseComponent />
         <div className="container px-4 md:px-0">
--- src/components/onthedaypage/WhatToExpectSection/index.jsx
+++ src/components/onthedaypage/WhatToExpectSection/index.jsx
@@ -61,8 +61,9 @@ const WhatToExpectSection = ({ onthedayContents }) => {
 
   return (
     <>
-      {/* <section className="pt-20 pb-20 bg-black relative overflow-hidden">
+      <section className="pt-20 pb-20 bg-black relative overflow-hidden">
         <NoiseComponent />
+
         <div className="container">
           <h3 className="text-white font-bold text-xl md:text-3xl text-center py-6">
             WHAT TO EXPECT FROM A NIGHT WITH THE WHITE ROOMS
@@ -70,19 +71,21 @@ const WhatToExpectSection = ({ onthedayContents }) => {
 
           {onthedayContents?.contents?.map((content, index) => {
             const isEven = index % 2 === 0;
+
             return (
               <div
                 key={content?._id}
-                className={`relative flex flex-col lg:block ${
-                  isEven ? "h-[700px]" : "h-[700px]"
-                } mb-10 lg:mb-0`}
+                className="relative flex flex-col lg:block h-auto lg:h-[700px] mb-16 lg:mb-0"
               >
+
                 <div
-                  className={`absolute ${
-                    isEven ? "top-[80px]" : "top-[56px]"
-                  } lg:top-[116px] ${
-                    isEven ? "right-8" : "right-5"
-                  } lg:right-[55px] w-[578px]`}
+                  className={`absolute mt-6 lg:mt-0
+    top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+    w-full max-w-[578px] mx-auto lg:mx-0
+    ${isEven
+                      ? "lg:top-[40px] lg:right-[45px] lg:left-auto lg:-translate-x-0 lg:-translate-y-0"
+                      : "lg:bottom-[40px] lg:left-[45px] lg:right-auto lg:-translate-x-0 lg:-translate-y-0"
+                    }`}
                 >
                   <Image
                     src="/assets/images/rw-bg.png"
@@ -94,10 +97,10 @@ const WhatToExpectSection = ({ onthedayContents }) => {
                   />
                 </div>
 
-                   <div
-                  className={`relative lg:absolute mb-4 lg:mb-0 w-full max-w-[650px] ${
-                    isEven ? "lg:top-0 lg:left-28" : "lg:top-32 lg:right-20"
-                  }`}
+
+                <div
+                  className={`relative mb-6 lg:absolute w-full max-w-[650px] mx-auto lg:mx-0
+              ${isEven ? "lg:top-0 lg:left-28" : "lg:top-32 lg:right-20"}`}
                 >
                   <Image
                     src={content?.section_image_url}
@@ -110,15 +113,15 @@ const WhatToExpectSection = ({ onthedayContents }) => {
                 </div>
 
                 <div
-                  className={`relative lg:absolute ${
-                    isEven ? "lg:bottom-[48px]" : "lg:top-[50px]"
-                  } ${
-                    isEven
-                      ? "lg:right-[-276px] lg:transform lg:-translate-x-1/2"
-                      : " lg:left-[453px] lg:transform lg:-translate-x-1/2"
-                  }   lg:w-[700px] py-[57px] px-10 lg:px-[91px] bg-[#1B1E25] z-10 shadow-xl`}
+                  className={`relative w-full max-w-[700px] mx-auto
+              py-8 px-6 bg-[#1B1E25] z-10 shadow-xl
+              lg:absolute lg:py-[57px] lg:px-[91px]
+              ${isEven
+                      ? "lg:bottom-[48px] lg:right-[-276px] lg:transform lg:-translate-x-1/2"
+                      : "lg:top-[50px] lg:left-[453px] lg:transform lg:-translate-x-1/2"
+                    }`}
                 >
-                  <h3 className="text-white font-bold text-xl md:text-3xl mb-8">
+                  <h3 className="text-white font-bold text-xl md:text-3xl mb-6 lg:mb-8">
                     {content?.title}
                   </h3>
 
@@ -137,160 +140,7 @@ const WhatToExpectSection = ({ onthedayContents }) => {
             );
           })}
         </div>
-      </section> */}
-      <section className="pt-20 pb-20 bg-black relative overflow-hidden">
-  <NoiseComponent />
-
-  <div className="container">
-    <h3 className="text-white font-bold text-xl md:text-3xl text-center py-6">
-      WHAT TO EXPECT FROM A NIGHT WITH THE WHITE ROOMS
-    </h3>
-
-    {onthedayContents?.contents?.map((content, index) => {
-      const isEven = index % 2 === 0;
-
-      return (
-        <div
-          key={content?._id}
-          className="relative flex flex-col lg:block h-auto lg:h-[700px] mb-16 lg:mb-0"
-        >
-   
-          <div
-            className={`relative mt-6 lg:absolute lg:mt-0 
-              ${isEven ? "lg:top-[116px]" : "lg:top-[116px]"}
-              ${isEven ? "lg:right-[55px]" : "lg:right-[55px]"}
-              w-full max-w-[578px] mx-auto lg:mx-0`}
-          >
-            <Image
-              src="/assets/images/rw-bg.png"
-              alt=""
-              width={578}
-              height={578}
-              className="w-full h-auto"
-              priority
-            />
-          </div>
-
-    
-          <div
-            className={`relative mb-6 lg:absolute w-full max-w-[650px] mx-auto lg:mx-0
-              ${isEven ? "lg:top-0 lg:left-28" : "lg:top-32 lg:right-20"}`}
-          >
-            <Image
-              src={content?.section_image_url}
-              alt={content?.title}
-              width={650}
-              height={650}
-              className="w-full h-auto object-cover img_border"
-              priority
-            />
-          </div>
-
-          <div
-            className={`relative w-full max-w-[700px] mx-auto
-              py-8 px-6 bg-[#1B1E25] z-10 shadow-xl
-              lg:absolute lg:py-[57px] lg:px-[91px]
-              ${
-                isEven
-                  ? "lg:bottom-[48px] lg:right-[-276px] lg:transform lg:-translate-x-1/2"
-                  : "lg:top-[50px] lg:left-[453px] lg:transform lg:-translate-x-1/2"
-              }`}
-          >
-            <h3 className="text-white font-bold text-xl md:text-3xl mb-6 lg:mb-8">
-              {content?.title}
-            </h3>
-
-            <div className="text-sm md:text-lg font-normal text-[#8F8F8F]">
-              {content?.description && (
-                <div
-                  className="description"
-                  dangerouslySetInnerHTML={{
-                    __html: parseDescription(content?.description),
-                  }}
-                />
-              )}
-            </div>
-          </div>
-        </div>
-      );
-    })}
-  </div>
-</section>
-
-      {/* <section className="pt-20 pb-20 bg-black relative overflow-hidden">
-        <NoiseComponent />
-        <div className="container px-4 md:px-0">
-          <h3 className="text-white font-bold text-xl md:text-3xl text-center py-6">
-            WHAT TO EXPECT FROM A NIGHT WITH THE WHITE ROOMS
-          </h3>
-
-          {onthedayContents?.contents?.map((content, index) => {
-            const isEven = index % 2 === 0;
-
-            return (
-              <div
-                key={content?._id}
-                className={`relative flex flex-col lg:block mb-10 lg:mb-0`}
-              >
-                <div
-                  className={`absolute top-[80px] lg:top-[116px] ${
-                    isEven
-                      ? "right-8 lg:right-[55px]"
-                      : "right-5 lg:right-[55px]"
-                  } w-full max-w-[578px]`}
-                >
-                  <Image
-                    src="/assets/images/rw-bg.png"
-                    alt=""
-                    width={578}
-                    height={578}
-                    className="w-full h-auto"
-                    priority
-                  />
-                </div>
-
-                <div
-                  className={`relative lg:absolute mb-4 lg:mb-0 w-full max-w-[428px] lg:max-w-[650px] ${
-                    isEven ? "lg:top-0 lg:left-28" : "lg:top-32 lg:right-20"
-                  } mx-auto`}
-                >
-                  <Image
-                    src={content?.section_image_url}
-                    alt={content?.title}
-                    width={650}
-                    height={650}
-                    className="w-full h-auto object-cover img_border"
-                    priority
-                  />
-                </div>
-
-                <div
-                  className={`relative lg:absolute w-full max-w-[700px] py-10 px-6 lg:px-[91px] bg-[#1B1E25] z-10 shadow-xl mx-auto ${
-                    isEven
-                      ? "lg:bottom-[48px] lg:right-[-276px] lg:transform lg:-translate-x-1/2"
-                      : "lg:top-[50px] lg:left-[453px] lg:transform lg:-translate-x-1/2"
-                  }`}
-                >
-                  <h3 className="text-white font-bold text-xl md:text-3xl mb-6 md:mb-8 text-center lg:text-left">
-                    {content?.title}
-                  </h3>
-
-                  <div className="text-sm md:text-lg font-normal text-[#8F8F8F] text-center lg:text-left">
-                    {content?.description && (
-                      <div
-                        className="description"
-                        dangerouslySetInnerHTML={{
-                          __html: parseDescription(content?.description),
-                        }}
-                      />
-                    )}
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </section> */}
+      </section>
     </>
   );
 };
