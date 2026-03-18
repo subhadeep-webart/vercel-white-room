bc76c80eb9b4acaa67d20f3c4247e397d6d8cf0b
subhadeep-webart <subhadeep.das@webart.technology>
2026-03-18 15:18:57 +0530

    Stiuff insta

--- src/components/homepage/InstagramFeedSection/InstagramFeed.jsx
+++ src/components/homepage/InstagramFeedSection/InstagramFeed.jsx
@@ -6,12 +6,12 @@ const InstagramFeed = ({ instagramPosts = [] }) => {
   console.log("Instagram Posts=====>", instagramPosts);
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-1">
-      {instagramPosts.slice(0,6).map((post) => (
+      {instagramPosts.slice(0, 6).map((post) => (
         <Link
           href={post.permalink}
           target="_blank"
           rel="noopener noreferrer"
-          className="!mb-4 !relative overflow-hidden aspect-w-4 aspect-h-5 group hover:scale-95 hover:opacity-90 transition-all delay-200  h-[200px] lg:h-[320px]"
+          className="!mb-4 !relative overflow-hidden group hover:scale-95 hover:opacity-90 transition-all delay-200  h-[200px] lg:h-[320px]"
           key={post.id}
         >
           {/* { ? (
@@ -20,17 +20,17 @@ const InstagramFeed = ({ instagramPosts = [] }) => {
               className="w-full h-[200px] lg:h-[320px] object-cover"
             />
           ) : ( */}
-            <Image
-              src={post?.media_type == "VIDEO"? post.thumbnail_url : post.media_url}
-                // src={post.thumbnail_url}
-              alt={post.caption || "Instagram Post"}
-              fill
-              className="object-cover"
-              sizes="(max-width: 640px) 30vw,
-                       (max-width: 1024px) 33vw,
-                       (max-width: 1280px) 25vw,
-                       16vw "
-            />
+          <Image
+            src={post?.media_type == "VIDEO" ? post.thumbnail_url : post.media_url}
+            // src={post.thumbnail_url}
+            alt={post.caption || "Instagram Post"}
+            fill
+            className="object-cover"
+          // sizes="(max-width: 640px) 40vw,
+          //            (max-width: 1024px) 33vw,
+          //            (max-width: 1280px) 25vw,
+          //            16vw "
+          />
           {/* )} */}
 
           <div className="absolute top-2 right-2 bg-white w-6 h-6 flex justify-center items-center rounded-full text-white">
